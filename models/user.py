f57c298ad2b6921ddf7258025d5a2959272d3b64
bneayoub <bne.ayoub@gmail.com>
2024-02-06 02:35:27 +0100

    Add User model implementation

---
+++ models/user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''
+    Implementation of the User class which inherits from BaseModel
+'''
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
+
+class User(BaseModel, Base):
+    '''
+        Definition of the User class
+    '''
+    __tablename__ = "users"
+    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
